22be82475a055983e3a075e916f472c05b08d7f7
farooq365 <91167693+farooq365@users.noreply.github.com>
2021-10-10 09:54:53 +0600

    calculate factorial of a number in recuresive way

--- calculateFactorialOfaNumber.js
+++ calculateFactorialOfaNumber.js
@@ -1,8 +1,14 @@
 function calculateFactorial(number) {
-    let factorial = 1;
-    for (let i = number; i > 1 ; i--) {
-        factorial = factorial*i;  
+    // let factorial = 1;
+    // for (let i = number; i > 1 ; i--) {
+    //     factorial = factorial*i;  
+    // }
+    // return factorial;   
+    if(number == 0){
+        return 1;
     }
-    return factorial;   
+    else {
+        return number*calculateFactorial(number -1);
+    }
 }
 console.log(calculateFactorial(5))
