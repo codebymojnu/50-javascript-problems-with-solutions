5c392816e6742c9293e15e0d5d2a40d876c2532b
farooq365 <91167693+farooq365@users.noreply.github.com>
2021-10-11 13:58:31 +0600

    remove duplicate from an array

---
+++ 11. remove duplicate.js
@@ -0,0 +1,9 @@
+const id = [10, 20, 30, 10, 40, 20, 30, 50, 20, 20];
+const newId = [];
+for (let i = 0; i < id.length; i++) {
+    const element = id[i];
+    if(newId.indexOf(element)== -1){
+        newId.push(id[i]);
+    }
+}
+console.log(newId);
