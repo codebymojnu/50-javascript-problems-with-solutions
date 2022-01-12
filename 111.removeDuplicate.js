f4e691a30af6d88ffb3f5768cd54a8d7170ab2cc
jsmojnu <learnwithmojnu@gmail.com>
2022-01-12 17:08:31 +0600

    remove duplicate number of an array

--- 111.removeDuplicate.js
+++ 111.removeDuplicate.js
@@ -2,7 +2,7 @@ const id = [10, 20, 30, 10, 40, 20, 30, 50, 20, 20];
 const newId = [];
 for (let i = 0; i < id.length; i++) {
     const element = id[i];
-    if(newId.indexOf(element)== -1){
+    if(newId.indexOf(element) === -1){
         newId.push(id[i]);
     }
 }
