5d97a107021104f4315146d6dd87a0113794998a
jsmojnu <learnwithmojnu@gmail.com>
2022-01-12 16:01:24 +0600

    calculate factorial of a number

--- 2.inchToFeet.js
+++
@@ -1,9 +0,0 @@
-function convertInchToFeet(inch){
-    const feet = inch/12;
-    return feet.toFixed(2);
-}
-const mojnuFeet = convertInchToFeet(62);
-console.log(mojnuFeet);
-
-const daduFeet = convertInchToFeet(48);
-console.log(daduFeet);
