075141bca5264927c5296464b6a15643caa079dd
jsmojnu <learnwithmojnu@gmail.com>
2022-01-12 17:31:48 +0600

    budget calculator

--- 117.budgetCalculator.js
+++ 117.budgetCalculator.js
@@ -1,8 +1,14 @@
 function budgetCalculator(ghori, phone, laptop){
-	if(ghori >=0 && phone >=0 && laptop >=0){
+	if(ghori === 0 && phone === 0 && laptop === 0){
+		return "You can not select all to 0";
+	}
+	else if(ghori >= 0 && phone >= 0 && laptop >= 0){
 		return ghori*50 + phone*100 + laptop*500;
 	}
 	else{
 		return "You entered negative number or string, check and please try again";
 	}
 }
+
+const total = budgetCalculator(0, 0, 1);
+console.log(total);
